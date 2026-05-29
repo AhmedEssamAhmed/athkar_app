828a5927c6393c11aaad9afc074da04fca5627e2
OmarElMaraghy <163595422+OmarElMaraghy@users.noreply.github.com>
2026-05-29 20:28:12 +0300

    Revert "- Cleaned PrayerTimeProvider fallback location handling."
    This reverts commit 7d4e8aaf22b4c6ef9725b38b74bb851cd51a8ab7.

--- lib/core/providers/prayer_time_provider.dart
+++ lib/core/providers/prayer_time_provider.dart
@@ -160,7 +160,6 @@ class PrayerTimeProvider extends ChangeNotifier {
       _error = e.toString();
       if (!_prayerService.isLocationSet) {
         _prayerService.setDefaultLocation();
-        _locationName = _prayerService.getCityName();
         _updatePrayerTimes();
       }
     } finally {
@@ -408,6 +407,7 @@ class PrayerTimeProvider extends ChangeNotifier {
     _locationName = await _resolveLocationName(lat, lng);
     _updatePrayerTimes();
     await _scheduleAllNotifications();
+    notifyListeners();
   }
 
   Future<String> _resolveLocationName(double lat, double lng) async {
@@ -444,6 +444,7 @@ class PrayerTimeProvider extends ChangeNotifier {
     _prayerService.setHijriMethod(method);
     _updatePrayerTimes();
     await _scheduleAllNotifications();
+    notifyListeners();
   }
 
   void setLanguage({required bool isArabic}) {
